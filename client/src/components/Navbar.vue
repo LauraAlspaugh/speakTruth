2bdc53fac0239af6ee5004bcc2a326b83737df92
Laura <lauraalspaugh2002@icloud.com>
2024-01-15 13:13:22 -0700

    about page finished

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-sm navbar-dark  px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <i class="mdi mdi-volume-high fs-1 photo-title text-dark "><span class="plan-title fs-2 text-dark p-0"
+        <i class="mdi mdi-home fs-1 photo-title text-dark "><span class="plan-title fs-2 text-dark p-0"
             title="back to homepage"></span></i>
       </div>
     </router-link>
@@ -11,14 +11,16 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
+      <!-- LOGIN COMPONENT HERE -->
       <ul class="navbar-nav me-auto">
+
         <li>
-          <!-- <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
+          <router-link :to="{ name: 'About' }" class="btn text-dark about-page fs-5 lighten-30 selectable text-uppercase">
             About
-          </router-link> -->
+          </router-link>
+
         </li>
       </ul>
-      <!-- LOGIN COMPONENT HERE -->
       <div>
         <button class="btn text-light" @click="toggleTheme"><i class="mdi"
             :class="theme == 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"></i></button>
@@ -65,8 +67,13 @@ a:hover {
   text-transform: uppercase;
 }
 
+.about-page {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+}
+
 .navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
+  border-bottom: 2px solid var(--bs-dark);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -1,6 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="container-fluid about-details">
+    <section class="row justify-content-evenly">
+      <div class="col-8 text-center">
+        <p class="fs-2 about-title">
+          Behind Speak Truth.
+        </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae voluptate, quae placeat totam nisi fugiat
+          cumque dignissimos accusantium reiciendis, accusamus rem autem velit voluptatibus eveniet tempora quis. Id
+          corrupti mollitia nihil maiores recusandae consequuntur eum a, quo enim accusantium! Reprehenderit eum tempore
+          excepturi possimus temporibus reiciendis, expedita dicta voluptates illum ducimus quis amet alias dolorem
+          exercitationem dignissimos deserunt autem iste blanditiis optio pariatur adipisci deleniti? Consectetur, ab
+          quod! Nulla quaerat iste voluptas quibusdam hic laudantium iure cum sequi nam reprehenderit repellat veniam
+          tenetur facere mollitia magnam, perspiciatis, blanditiis magni doloremque tempora molestias quis est. Possimus,
+          quibusdam excepturi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo et porro quia, illum nemo perferendis doloremque
+          veniam dolore rerum? Iste est fugit sunt, iure voluptate nemo hic autem officiis omnis magni neque ipsa minima
+          quisquam accusantium unde aut maiores magnam velit expedita obcaecati nesciunt molestiae quidem. Tempore quidem
+          provident fugit ipsum ipsam eos cupiditate consectetur ab officia sunt, odit unde nesciunt vel qui perspiciatis
+          nemo explicabo aspernatur excepturi debitis ex? Praesentium nihil repellat explicabo nisi. Eum, veritatis
+          necessitatibus cupiditate dicta aliquam quasi eligendi. Accusantium, similique.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus laudantium nisi autem expedita neque
+          hic nobis odio maxime architecto non similique dolorum fugit, quisquam reiciendis tempora, dignissimos quas
+          excepturi ratione in ipsa vitae? Aperiam nostrum eaque, exercitationem eos laborum architecto enim, velit
+          officia provident blanditiis aspernatur.</p>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -13,3 +40,15 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+.about-title {
+  font-family: 'Pinyon Script', cursive;
+
+}
+
+.about-details {
+  background-color: light gray;
+  border-radius: 7px;
+  backdrop-filter: blur(12px);
+}
+</style>
